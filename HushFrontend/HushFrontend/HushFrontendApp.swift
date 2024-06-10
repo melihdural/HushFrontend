27161f8c9e1c954fab22e779facc6b3033756e6d
melihdural <duralmelih@gmail.com>
2024-06-10 23:14:02 +0300

    Swift app init

---
+++ HushFrontend/HushFrontend/HushFrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  HushFrontendApp.swift
+//  HushFrontend
+//
+//  Created by Melih Dural on 10.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct HushFrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
